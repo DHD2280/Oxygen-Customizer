f2ba964b50017778b1f5640def566e54de439975
DHD2280 <luigiconte98@gmail.com>
2024-04-22 09:32:59 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "it.dhd.oxygencustomizer"
         minSdk = 33
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         setProperty("archivesBaseName", "Oxygen Customizer v$versionName")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
