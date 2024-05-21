7c1148f717cf9865b6bc526a1452607dd69aa447
DHD2280 <luigiconte98@gmail.com>
2024-05-21 22:53:45 +0200

    Removed unused methods

--- app/src/main/java/it/dhd/oxygencustomizer/utils/ThemeUtils.java
+++ app/src/main/java/it/dhd/oxygencustomizer/utils/ThemeUtils.java
@@ -23,56 +23,11 @@ public class ThemeUtils {
         return typedValue.data;
     }
 
-    public static @ColorInt int getOnPrimaryColor(Context context) {
-        TypedValue typedValue = new TypedValue();
-        Resources.Theme theme = context.getTheme();
-        theme.resolveAttribute(com.google.android.material.R.attr.colorOnPrimary, typedValue, true);
-        return typedValue.data;
-    }
-
     public static @ColorInt int getBackgroundColor() {
         TypedValue typedValue = new TypedValue();
         Resources.Theme theme = getAppContext().getTheme();
         theme.resolveAttribute(com.google.android.material.R.attr.backgroundColor, typedValue, true);
         return typedValue.data;
-    }
-
-    public static @ColorInt int getOnBackgroundColor() {
-        TypedValue typedValue = new TypedValue();
-        Resources.Theme theme = getAppContext().getTheme();
-        theme.resolveAttribute(com.google.android.material.R.attr.colorOnBackground, typedValue, true);
-        return typedValue.data;
-    }
-
-    public static @ColorInt int getColorSurfaceHighest() {
-        TypedValue typedValue = new TypedValue();
-        Resources.Theme theme = getAppContext().getTheme();
-        theme.resolveAttribute(com.google.android.material.R.attr.colorSurfaceContainerHighest, typedValue, true);
-        return typedValue.data;
-    }
-
-    public static @ColorInt int getColorSurfaceHigh() {
-        TypedValue typedValue = new TypedValue();
-        Resources.Theme theme = getAppContext().getTheme();
-        theme.resolveAttribute(com.google.android.material.R.attr.colorSurfaceContainerHigh, typedValue, true);
-        return typedValue.data;
-    }
-
-    public static @ColorInt int getColorSurfaceContainer() {
-        TypedValue typedValue = new TypedValue();
-        Resources.Theme theme = getAppContext().getTheme();
-        theme.resolveAttribute(com.google.android.material.R.attr.colorSurfaceContainer, typedValue, true);
-        return typedValue.data;
-    }
-
-    public static int getColorResCompat(Context context, @AttrRes int id) {
-        TypedValue typedValue = new TypedValue();
-        Resources.Theme theme = context.getTheme();
-        theme.resolveAttribute(id, typedValue, false);
-        TypedArray arr = context.obtainStyledAttributes(typedValue.data, new int[]{id});
-        @ColorInt int color = arr.getColor(0, -1);
-        arr.recycle();
-        return color;
     }
 
 }
