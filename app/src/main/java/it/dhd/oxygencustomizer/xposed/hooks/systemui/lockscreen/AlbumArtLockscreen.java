f0f36882f7b9f56034a71529dac118bbc6a1f1a7
DHD2280 <luigiconte98@gmail.com>
2024-06-20 00:38:57 +0200

    Improve Lockscreen Media Art

--- app/src/main/java/it/dhd/oxygencustomizer/xposed/hooks/systemui/lockscreen/AlbumArtLockscreen.java
+++ app/src/main/java/it/dhd/oxygencustomizer/xposed/hooks/systemui/lockscreen/AlbumArtLockscreen.java
@@ -8,52 +8,39 @@ import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 import static it.dhd.oxygencustomizer.utils.Constants.Packages.SYSTEM_UI;
 import static it.dhd.oxygencustomizer.xposed.XPrefs.Xprefs;
+import static it.dhd.oxygencustomizer.xposed.hooks.systemui.AudioDataProvider.getArt;
 import static it.dhd.oxygencustomizer.xposed.hooks.systemui.AudioDataProvider.getMediaMetadata;
 import static it.dhd.oxygencustomizer.xposed.hooks.systemui.AudioDataProvider.getPlaybackState;
 import static it.dhd.oxygencustomizer.xposed.hooks.systemui.OpUtils.getPrimaryColor;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.ImageDecoder;
-import android.graphics.drawable.AnimatedImageDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.media.MediaMetadata;
+import android.graphics.drawable.TransitionDrawable;
 import android.media.session.PlaybackState;
-import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
-import androidx.core.content.res.ResourcesCompat;
-
-import java.io.File;
-
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import it.dhd.oxygencustomizer.BuildConfig;
-import it.dhd.oxygencustomizer.R;
 import it.dhd.oxygencustomizer.xposed.XposedMods;
 import it.dhd.oxygencustomizer.xposed.hooks.systemui.AudioDataProvider;
 import it.dhd.oxygencustomizer.xposed.utils.DrawableConverter;
 import it.dhd.oxygencustomizer.xposed.utils.SystemUtils;
-import it.dhd.oxygencustomizer.xposed.views.VisualizerView;
-import it.dhd.oxygencustomizer.xposed.views.pulse.PulseControllerImpl;
 
 public class AlbumArtLockscreen extends XposedMods {
 
     private static final String listenPackage = SYSTEM_UI;
+    private Bitmap mArt;
     private boolean showAlbumArt = true;
-    private int albumArtFilter = 0; // 0 = None, 1 = Blur, 2 = Darken, 3 = Lighten
+    private int albumArtFilter = 0;
     private float albumArtBlurAmount = 7.5f;
     private FrameLayout albumArtContainer;
     private ImageView albumArtView;
     private Object mScrimController;
-    private MediaMetadata mMediaMetaData;
-    private Bitmap mAlbumArt;
     private boolean shouldShowArt = false;
 
     public AlbumArtLockscreen(Context context) {
@@ -115,23 +102,11 @@ public class AlbumArtLockscreen extends XposedMods {
             }
         });
 
-        // Get Scrim Change
-        Class<?> ScrimControllerClass = findClass("com.android.systemui.statusbar.phone.ScrimController", lpparam.classLoader);
-        hookAllMethods(ScrimControllerClass, "applyAndDispatchState", new XC_MethodHook() {
+        hookAllMethods(CentralSurfacesImplClass, "onKeyguardGoingAway", new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                shouldShowArt = false;
                 updateAlbumArt();
-            }
-        });
-
-        Class<?> QSImplClass = findClass("com.android.systemui.qs.QSFragment", lpparam.classLoader);
-        hookAllMethods(QSImplClass, "setQsExpansion", new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                if((boolean) callMethod(param.thisObject, "isKeyguardState"))
-                {
-                    updateAlbumArt();
-                }
             }
         });
 
@@ -155,27 +130,18 @@ public class AlbumArtLockscreen extends XposedMods {
         }
         boolean isPlaying = (isMusicActive || state == PlaybackState.STATE_PLAYING);
         if (showAlbumArt && isPlaying) {
-            if (mMediaMetaData != getMediaMetadata()) {
-                mMediaMetaData = getMediaMetadata();
-                mAlbumArt = getArt(mMediaMetaData);
-            }
-            Bitmap filteredArt = getArtFilter(mAlbumArt);
-            albumArtView.setImageBitmap(filteredArt);
+            Bitmap oldArt = mArt;
+            mArt = getArtFilter(getArt());
+            Drawable[] layers = new Drawable[]{
+                    new BitmapDrawable(mContext.getResources(), oldArt),
+                    new BitmapDrawable(mContext.getResources(), mArt)};
+            TransitionDrawable transitionDrawable = new TransitionDrawable(layers);
+            albumArtView.setImageDrawable(transitionDrawable);
+            transitionDrawable.startTransition(250);
         } else {
             albumArtView.setImageBitmap(null);
         }
         updateAlbumArt();
-    }
-
-    private Bitmap getArt(MediaMetadata mediaMetadata) {
-        if (mediaMetadata == null) {
-            return null;
-        }
-        Bitmap art = mediaMetadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART);
-        if (art == null) {
-            return mediaMetadata.getBitmap(MediaMetadata.METADATA_KEY_ART);
-        }
-        return art;
     }
     
     private Bitmap getArtFilter(Bitmap art) {
@@ -183,10 +149,8 @@ public class AlbumArtLockscreen extends XposedMods {
         switch (albumArtFilter) {
             default -> finalArt = art;
             case 1 -> finalArt = DrawableConverter.toGrayscale(art);
-            case 2 -> {
-                finalArt = DrawableConverter.getColoredBitmap(new BitmapDrawable(mContext.getResources(), art),
-                        getPrimaryColor(mContext));
-            }
+            case 2 -> finalArt = DrawableConverter.getColoredBitmap(new BitmapDrawable(mContext.getResources(), art),
+                    getPrimaryColor(mContext));
             case 3 -> finalArt = DrawableConverter.getBlurredImage(mContext, art, albumArtBlurAmount);
             case 4 -> finalArt = DrawableConverter.getGrayscaleBlurredImage(mContext, art, albumArtBlurAmount);
         };
