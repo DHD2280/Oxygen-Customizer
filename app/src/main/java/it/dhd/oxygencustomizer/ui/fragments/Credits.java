fddbe336683a9162da565925628fdc7949ea48f9
DHD2280 <luigiconte98@gmail.com>
2024-08-20 23:31:00 +0200

    Added Contributor Image

--- app/src/main/java/it/dhd/oxygencustomizer/ui/fragments/Credits.java
+++ app/src/main/java/it/dhd/oxygencustomizer/ui/fragments/Credits.java
@@ -74,7 +74,7 @@ public class Credits extends BaseFragment {
         credits.add(new CreditsModel(VIEW_TYPE_ITEM, "Max", "", "", R.drawable.ic_default_person));
         credits.add(new CreditsModel(VIEW_TYPE_ITEM, "Siri00", "", "", R.drawable.ic_default_person));
         credits.add(new CreditsModel(VIEW_TYPE_ITEM, "Pasqui1978", "", "", R.drawable.ic_default_person));
-        credits.add(new CreditsModel(VIEW_TYPE_ITEM, "ZioProne", "", "https://github.com/Pronegate", R.drawable.ic_default_person));
+        credits.add(new CreditsModel(VIEW_TYPE_ITEM, "ZioProne", "", "https://github.com/Pronegate", ResourcesCompat.getDrawable(getResources(), R.drawable.zioprone, requireContext().getTheme())));
         credits.add(new CreditsModel(VIEW_TYPE_ITEM, "ℤ\uD835\uDD56\uD835\uDD5F\uD835\uDD60 \uD835\uDD4F", "OOS 13 Tester", "", R.drawable.ic_default_person));
 
         credits.add(new CreditsModel("Translators"));
