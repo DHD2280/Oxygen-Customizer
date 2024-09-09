7ee5b9ff488f5875203880f6b16992ffbf76415a
DHD2280 <luigiconte98@gmail.com>
2024-09-10 01:08:00 +0200

    Revert AGP 8.6.0, return to 8.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,6 @@ android {
         release {
             isMinifyEnabled = false
             isShrinkResources = false
-            isCrunchPngs = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.6.0" apply false
+    id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
 val commonsTextVersion by extra("1.12.0")
