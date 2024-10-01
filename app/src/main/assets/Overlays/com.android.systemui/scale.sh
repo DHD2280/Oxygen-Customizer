7a22bdad90f92bac0563c9938cf58f5e2b9d2775
DHD2280 <luigiconte98@gmail.com>
2024-10-01 23:10:41 +0200

    Remove unused stuff

--- app/src/main/assets/Overlays/com.android.systemui/scale.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find . -name "$1*" -exec sed -i -e 's/24.0dip/36.0dp/g' {} \;
-find . -name "$1*" -exec sed -i -e 's/24.0dp/36.0dp/g' {} \;
-find "$PWD" -name "$1*" -exec sed -i -e 's/24.0dip/36.0dp/g' {} \;
-find "$PWD" -name "$1*" -exec sed -i -e 's/24.00dp/36.0dp/g' {} \;
-find "$PWD" -name "$1*" -exec sed -i -e 's/24dp/36.0dp/g' {} \;
