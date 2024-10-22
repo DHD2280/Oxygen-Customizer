68d82db8304e076d400800985b8617c1ee8d8507
DHD2280 <luigiconte98@gmail.com>
2024-10-22 22:49:12 +0200

    Added Oplus Dialogs themed in Notification Styles
    Co-authored-by: tugaia56 <tugaia56@gmail.com>

--- app/src/main/java/it/dhd/oxygencustomizer/utils/ModuleConstants.java
+++ app/src/main/java/it/dhd/oxygencustomizer/utils/ModuleConstants.java
@@ -9,7 +9,7 @@ import it.dhd.oxygencustomizer.OxygenCustomizer;
 
 public class ModuleConstants {
 
-    public final static String MODULE_VERSION_NAME = "0.0.43";
+    public final static String MODULE_VERSION_NAME = "0.0.44";
     public final static int MODULE_VERSION_CODE = 1;
 
     // Storage location
