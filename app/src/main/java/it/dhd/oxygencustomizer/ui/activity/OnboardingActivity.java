83f07f507635c7911350cbcbfa69e2daf4b3ed66
DHD2280 <luigiconte98@gmail.com>
2025-01-24 09:53:58 +0100

    OnboardingActivity: fix window insets for edge to edge

--- app/src/main/java/it/dhd/oxygencustomizer/ui/activity/OnboardingActivity.java
+++ app/src/main/java/it/dhd/oxygencustomizer/ui/activity/OnboardingActivity.java
@@ -3,10 +3,14 @@ package it.dhd.oxygencustomizer.ui.activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.OnBackPressedCallback;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.color.DynamicColors;
 
@@ -30,6 +34,23 @@ public class OnboardingActivity extends OplusActivity {
 
         binding = ActivityOnboardingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            // Apply the insets as a margin to the view. This solution sets only the
+            // bottom, left, and right dimensions, but you can apply whichever insets are
+            // appropriate to your layout. You can also update the view padding if that's
+            // more appropriate.
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            mlp.leftMargin = insets.left;
+            mlp.bottomMargin = insets.bottom;
+            mlp.rightMargin = insets.right;
+            v.setLayoutParams(mlp);
+
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            return WindowInsetsCompat.CONSUMED;
+        });
 
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
