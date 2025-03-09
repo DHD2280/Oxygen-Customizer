3f6ce41d1b1f3edfd25825a341135fa08b1066b4
DHD2280 <luigiconte98@gmail.com>
2025-03-09 15:25:29 +0100

    Added launcher themed icons option #67.
    Signed-off-by: DHD2280 <luigiconte98@gmail.com>

--- app/src/main/java/it/dhd/oxygencustomizer/xposed/hooks/launcher/ThemedIcons.java
+++ app/src/main/java/it/dhd/oxygencustomizer/xposed/hooks/launcher/ThemedIcons.java
@@ -1,26 +1,57 @@
 package it.dhd.oxygencustomizer.xposed.hooks.launcher;
 
+import static org.lsposed.hiddenapibypass.HiddenApiBypass.newInstance;
 import static de.robv.android.xposed.XposedBridge.hookAllConstructors;
 import static de.robv.android.xposed.XposedBridge.hookAllMethods;
+import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.getAdditionalInstanceField;
+import static de.robv.android.xposed.XposedHelpers.getBooleanField;
 import static de.robv.android.xposed.XposedHelpers.getIntField;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
+import static de.robv.android.xposed.XposedHelpers.getStaticIntField;
 import static de.robv.android.xposed.XposedHelpers.setAdditionalInstanceField;
 import static it.dhd.oxygencustomizer.utils.Constants.Packages.LAUNCHER;
 import static it.dhd.oxygencustomizer.xposed.XPrefs.Xprefs;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.drawable.AdaptiveIconDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import it.dhd.oxygencustomizer.xposed.XposedMods;
 import it.dhd.oxygencustomizer.xposed.utils.GoogleMonochromeIconFactory;
+import it.dhd.oxygencustomizer.xposed.utils.toolkit.ReflectedClass;
 
 public class ThemedIcons extends XposedMods {
 
     private final static String listenPackage = LAUNCHER;
     private final static String TAG = "Oxygen Customizer - Launcher - ThemedIcons: ";
+    private Set<String> mAllowedMonochrome = new HashSet<>();
+    Set<String> defaultValues = new HashSet<>(Arrays.asList("0", "1", "2", "3", "4"));
+    private boolean mDrawerMonochrome = false;
+    private boolean mWorkspaceMonochrome = false;
+    private boolean mFolderMonochrome = false;
+    private boolean mSearchMonochrome = false;
+    private boolean mTaskbarMonochrome = false;
+
+    private static final int DISPLAY_ALL_APPS = 1;
+    private static final int DISPLAY_FOLDER = 2;
+    private static final int DISPLAY_SEARCH_RESULT = 6;
+    private static final int DISPLAY_SEARCH_RESULT_SMALL = 7;
+    private static final int DISPLAY_TASKBAR = 5;
+    private static final int DISPLAY_WORKSPACE = 0;
+
+    private Object mIconProvider = null;
+    private Object mIconObserver = null;
 
     private static boolean ForceThemedLauncherIcons = false;
     private int mIconBitmapSize;
@@ -32,6 +63,18 @@ public class ThemedIcons extends XposedMods {
     @Override
     public void updatePrefs(String... Key) {
         ForceThemedLauncherIcons = Xprefs.getBoolean("force_themed_launcher_icons", false);
+        mAllowedMonochrome = Xprefs.getStringSet("themed_icons_where", defaultValues);
+        mDrawerMonochrome = mAllowedMonochrome.contains("1");
+        mWorkspaceMonochrome = mAllowedMonochrome.contains("0");
+        mFolderMonochrome = mAllowedMonochrome.contains("2");
+        mSearchMonochrome = mAllowedMonochrome.contains("3");
+        mTaskbarMonochrome = mAllowedMonochrome.contains("4");
+
+        if (Key.length > 0 &&
+                (Key[0].equals("force_themed_launcher_icons") || Key[0].equals("themed_icons_where"))) {
+            updateIcons();
+        }
+
     }
 
     @Override
@@ -45,17 +88,6 @@ public class ThemedIcons extends XposedMods {
                     mIconBitmapSize = getIntField(param.thisObject, "mIconBitmapSize");
                 }
             });
-
-//            Class<?> UxCustomAdaptiveIconDrawable = findClass("com.oplus.uxicon.ui.ui.UxCustomAdaptiveIconDrawable", lpparam.classLoader);
-//            hookAllConstructors(UxCustomAdaptiveIconDrawable, new XC_MethodHook() {
-//                @Override
-//                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-//                    for (int i = 0;i< param.args.length; i++) {
-//                        log("UxCustomAdaptiveIconDrawable: " + i + " " + (param.args[i] != null));
-//                    }
-//                    //log("UxCustomAdaptiveIconDrawable: " + callMethod(icon, "toString"));
-//                }
-//            });
 
             hookAllMethods(AdaptiveIconDrawable.class, "getMonochrome", new XC_MethodHook() {
                 @Override
@@ -82,6 +114,84 @@ public class ThemedIcons extends XposedMods {
         } catch (Throwable t) {
             log(t.getMessage());
         }
+
+//        ReflectedClass BitmapInfo = ReflectedClass.of("com.android.launcher3.icons.BitmapInfo");
+//        BitmapInfo
+//                .before("newIcon")
+//                .run(param -> {
+//                    if (!ForceThemedLauncherIcons) return;
+//                    Context context = (Context) param.args[0];
+//                    int flags = (int) param.args[1];
+//                    if ((flags & 1) == 0) return;
+//                    boolean isLowRes = (boolean) callMethod(param.thisObject, "isLowRes");
+//                    Bitmap mMono = (Bitmap) getObjectField(param.thisObject, "mMono");
+//                    Object FastDrawable;
+//                    GoogleMonochromeIconFactory mono = null;
+//                    if (mMono == null) {
+//                        mono = new GoogleMonochromeIconFactory((AdaptiveIconDrawable) param.thisObject, mIconBitmapSize);
+//                    }
+//                    param.setResult(mono);
+//
+//                });
+
+
+        ReflectedClass LauncherAppState = ReflectedClass.of("com.android.launcher3.LauncherAppState");
+        LauncherAppState
+                .afterConstruction()
+                .run(param -> {
+                    mIconProvider = getObjectField(param.thisObject, "mIconProvider");
+                });
+        ReflectedClass LauncherIconObserver = ReflectedClass.of("com.android.launcher3.LauncherAppState$IconObserver");
+        LauncherIconObserver
+                .afterConstruction()
+                .run(param -> mIconObserver = param.thisObject);
+
+        ReflectedClass OplusBubbleTextView = ReflectedClass.of("com.android.launcher3.OplusBubbleTextView");
+        OplusBubbleTextView
+                .after("applyIconAndLabel")
+                .run(param -> {
+                    Object itemInfoWithIcon = param.args[0];
+                    if (itemInfoWithIcon == null) return;
+                    int mDisplay = getIntField(param.thisObject, "mDisplay");
+                    boolean isIconThemed = getBooleanField(itemInfoWithIcon, "mIsIconEdited");
+                    if (isIconThemed) return;
+                    XposedBridge.log("ThemedIcons itemType = " + getIntField(itemInfoWithIcon, "itemType"));
+                    // ((mContext,
+                    // (Themes.isThemedIconEnabled(getContext())
+                    // && (i9 == 0 || i9 == 2 || i9 == 5 || i9 == 8 || i9 == 1 || i9 == 9)
+                    // && !itemInfoWithIcon.mIsIconEdited) ? 1 : 0
+                    Object newIcon = callMethod(itemInfoWithIcon, "newIcon", mContext, shouldUseTheme(mDisplay) ? 1 : 0);
+                    callMethod(param.thisObject, "setIcon", newIcon);
+                });
+
+        // Folder Previews
+        ReflectedClass PreviewItemManager = ReflectedClass.of("com.android.launcher3.folder.PreviewItemManager");
+        PreviewItemManager
+                .before("getNewIcon")
+                .run(param -> {
+                    int theme = 1;
+                    if (!mWorkspaceMonochrome) {
+                        theme = 0;
+                    }
+                    param.args[1] = theme;
+                });
+    }
+
+    private boolean shouldUseTheme(int display) {
+        return switch (display) {
+            case DISPLAY_ALL_APPS -> mDrawerMonochrome;
+            case DISPLAY_FOLDER -> mFolderMonochrome;
+            case DISPLAY_SEARCH_RESULT, DISPLAY_SEARCH_RESULT_SMALL -> mSearchMonochrome;
+            case DISPLAY_TASKBAR -> mTaskbarMonochrome;
+            default -> mWorkspaceMonochrome;
+        };
+    }
+
+    private void updateIcons() {
+        try {
+            String systemIconState = (String) callMethod(mIconProvider, "getSystemIconState");
+            callMethod(mIconObserver, "onSystemIconStateChanged", systemIconState);
+        } catch (Throwable ignored) {}
     }
 
     @Override
