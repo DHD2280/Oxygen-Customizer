ce5e8d407585bfd50dc8dbe2301aa6783b519715
Tullio <46009615+tugaia56@users.noreply.github.com>
2025-03-30 11:52:15 +0200

    Commit: DarkShadowUtils

--- app/src/main/java/it/dhd/oxygencustomizer/utils/DarkShadowUtils.java
+++ app/src/main/java/it/dhd/oxygencustomizer/utils/DarkShadowUtils.java
@@ -52,14 +52,14 @@ public final class DarkShadowUtils {
                                 "primary_dark_material_dark", "primary_material_dark"));
                     }},
                     Map.of(
-                            "background_floating_material_dark", 10,
-                            "background_holo_dark", 25,
-                            "background_leanback_dark", 30,
-                            "button_material_dark", -5,
-                            "button_material_light", -15,
-                            "holo_light_primary_dark", -10,
-                            "holo_primary", -15,
-                            "holo_primary_dark", -5
+                            "background_floating_material_dark", 25,
+                            "button_material_dark", 30,
+                            "holo_primary", 35,
+                            "holo_light_primary_dark", 40,
+                            "button_material_light", 45,
+                            "holo_primary_dark", 50,
+                            "background_holo_dark", 55,
+                            "background_leanback_dark", 60
                     ),
                     new ArrayList<>() {{
                         add("android");
@@ -68,7 +68,7 @@ public final class DarkShadowUtils {
             );
 
     public static int getColor(DarkShadowItem darkShadowItem) {
-        Log.w("DarkShadowUtils", "getColor: " + darkShadowItem.toString());
+        Log.w("DarkShadowUtils", "getColor: " + darkShadowItem.toString() + " - Color: " + OCPreferences.getInt("DST" + darkShadowItem.getOverlayName(), darkShadowItem.getColor()));
         return OCPreferences.getInt("DST" + darkShadowItem.getOverlayName(), darkShadowItem.getColor());
     }
 
