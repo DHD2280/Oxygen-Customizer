997f5e777732ea3b16b6fa447234c14d74124080
Tullio <46009615+tugaia56@users.noreply.github.com>
2025-04-22 11:19:17 +0200

    Commit files for Dark Shadow

--- app/src/main/java/it/dhd/oxygencustomizer/ui/fragments/uistyle/DarkShadowThemeFragment.java
+++ app/src/main/java/it/dhd/oxygencustomizer/ui/fragments/uistyle/DarkShadowThemeFragment.java
@@ -3,7 +3,7 @@ package it.dhd.oxygencustomizer.ui.fragments.uistyle;
 import static it.dhd.oxygencustomizer.utils.DarkShadowUtils.ACCENT1;
 import static it.dhd.oxygencustomizer.utils.DarkShadowUtils.ACCENT2;
 import static it.dhd.oxygencustomizer.utils.DarkShadowUtils.ACCENT3;
-import static it.dhd.oxygencustomizer.utils.DarkShadowUtils.BACKGROUND;
+//import static it.dhd.oxygencustomizer.utils.DarkShadowUtils.BACKGROUND;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -47,7 +47,7 @@ public class DarkShadowThemeFragment extends BaseFragment {
         add(ACCENT1);
         add(ACCENT2);
         add(ACCENT3);
-        add(BACKGROUND);
+//        add(BACKGROUND);
     }};
 
     private FragmentAppListBinding binding;
@@ -83,7 +83,7 @@ public class DarkShadowThemeFragment extends BaseFragment {
             }
         });
         binding.appFunctionSwitch.setSwitchChecked(
-                OverlayUtil.isOverlayDisabled("OxygenCustomizerComponent" + overlays[0] + Build.VERSION.SDK_INT + ".overlay")
+                OverlayUtil.isOverlayEnabled("OxygenCustomizerComponent" + overlays[0] + Build.VERSION.SDK_INT + ".overlay")
         );
 
         // RecyclerView
