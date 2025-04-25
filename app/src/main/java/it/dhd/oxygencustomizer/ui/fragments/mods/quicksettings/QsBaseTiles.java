ab8c82d38a0239c1a0974efae1fdc6b7e16eb15f
DHD2280 <luigiconte98@gmail.com>
2025-04-25 19:24:17 +0200

    Added fragments
    Signed-off-by: DHD2280 <luigiconte98@gmail.com>

---
+++ app/src/main/java/it/dhd/oxygencustomizer/ui/fragments/mods/quicksettings/QsBaseTiles.java
@@ -0,0 +1,33 @@
+package it.dhd.oxygencustomizer.ui.fragments.mods.quicksettings;
+
+import static it.dhd.oxygencustomizer.utils.Constants.Packages.SYSTEM_UI;
+
+import it.dhd.oxygencustomizer.R;
+import it.dhd.oxygencustomizer.ui.base.ControlledPreferenceFragmentCompat;
+
+public class QsBaseTiles extends ControlledPreferenceFragmentCompat {
+    @Override
+    public String getTitle() {
+        return getString(R.string.qs_base_tile);
+    }
+
+    @Override
+    public boolean backButtonEnabled() {
+        return true;
+    }
+
+    @Override
+    public int getLayoutResource() {
+        return R.xml.qs_base_tiles_prefs;
+    }
+
+    @Override
+    public boolean hasMenu() {
+        return true;
+    }
+
+    @Override
+    public String[] getScopes() {
+        return new String[]{SYSTEM_UI};
+    }
+}
