e28a89cfd79add21711c91cd5f9c879f1137411c
DHD2280 <luigiconte98@gmail.com>
2025-05-16 19:10:04 +0200

    QsWeatherWidget: optimize imports

--- app/src/main/java/it/dhd/oxygencustomizer/xposed/views/controls/weather/QsWeatherWidget.java
+++ app/src/main/java/it/dhd/oxygencustomizer/xposed/views/controls/weather/QsWeatherWidget.java
@@ -1,24 +1,7 @@
 package it.dhd.oxygencustomizer.xposed.views.controls.weather;
 
-import static de.robv.android.xposed.XposedBridge.log;
-import static it.dhd.oxygencustomizer.xposed.ResourceManager.modRes;
-import static it.dhd.oxygencustomizer.xposed.utils.ViewHelper.dp2px;
-
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import it.dhd.oxygencustomizer.BuildConfig;
-import it.dhd.oxygencustomizer.R;
-import it.dhd.oxygencustomizer.weather.OmniJawsClient;
-import it.dhd.oxygencustomizer.xposed.utils.ViewHelper;
 
 @SuppressWarnings("viewConstructor")
 public class QsWeatherWidget extends LinearLayout {
